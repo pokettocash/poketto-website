5e0a5130aab766a093083c46f126cb7fafc5b751
Jack Veiga <hello@jackveiga.com>
2019-04-27 23:31:14 +0100

    Fix links

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,15 +20,19 @@ const IndexPage = () => (
           <PhoneBackground>
             <Anime
             opacity={[0, 1]}
-            translateX={'1em'}
+            translateX={'3em'}
             easing={'easeInCubic'}
             delay={(e, i) => i * 1000}>
-              <div class="w-60 mw6">
+              <div class="w-60 mw6 nl5">
                 <Image />
               </div>
             </Anime>
           </PhoneBackground>
         </div>
+        <Anime
+          opacity={[0, 1]}
+          easing={'easeInCubic'}
+          delay={(e, i) => i * 1000}>
         <div class="w-50 v-mid ph5">
           <div class="flex">
             <div class="">
@@ -41,29 +45,34 @@ const IndexPage = () => (
           <div class="mw8">
             <p class="mt4 mw6 f5 lh-copy gt-walsheim">Poketto is a xDai pocket wallet for your day-to-day transactions. The simplest, most secure and near instant way to send money to friends and pay for your every day items.</p>
             <div class="flex-column">
+              <a href="https://testflight.com" target="_blank" rel="noopener noreferrer" class="black no-underline">
               <div class="flex">
                 <div class="mt3 flex items-center justify-center link dim ph4 dib bg-lightest-blue ba bw1 br3 b--lightest-blue pointer w-75">
                   <IosIcon />
-                  <p class="ml3 f5 gt-walsheim-medium">Test it now with Testflight</p>
+                  <p class="ml3 f5 black no-underline gt-walsheim-medium">Test it now with Testflight</p>
                 </div>
               </div>
-              <div class="flex">
-                <div class="mt3 flex items-center justify-center link dim ph4 dib bg-near-white ba bw1 br3 b--near-white pointer w-75">
-                  <XdaiIcon />
-                  <p class="ml3 f5 gt-walsheim-medium">Convert Ethereum to xDai</p>
+              </a>
+              <a href="https://medium.com/@jaredstauffer/how-to-get-xdai-how-to-convert-dai-to-xdai-eth-dai-xdai-30a60e4b6641" target="_blank" rel="noopener noreferrer" class="black no-underline">
+                <div class="flex">
+                  <div class="mt3 flex items-center justify-center link dim ph4 dib bg-near-white ba bw1 br3 b--near-white pointer w-75">
+                    <XdaiIcon />
+                    <p class="ml3 f5 gt-walsheim-medium">Convert Ethereum to xDai</p>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
             <div class="mt3 pt4 pt3 flex justify-start o-90">
-              <p class="mr3 f5 no-underline underline-hover hover-orange link pointer gt-walsheim">Twitter</p>
-              <p class="mr3 f5 no-underline underline-hover hover-orange link pointer gt-walsheim">Discord</p>
-              <p class="f5 no-underline underline-hover hover-orange link pointer gt-walsheim">Github</p>
+              <a href="https://www.twitter.com/pokettocash" target="_blank" rel="noopener noreferrer" class="mr3 f5 black no-underline underline-hover hover-orange link pointer gt-walsheim">Twitter</a>
+              <a href="https://discord.gg/kMTUpME" target="_blank" rel="noopener noreferrer" class="mr3 f5 black no-underline underline-hover hover-orange link pointer gt-walsheim">Discord</a>
+              <a href="https://github.com/pokettocash" target="_blank" rel="noopener noreferrer" class="f5 black no-underline underline-hover hover-orange link pointer gt-walsheim">Github</a>
             </div>
             <div class="mt2">
               <p class="f5 o-90 gt-walsheim">Contribute at <a href="https://www.donesunday.com/" target="_blank" rel="noopener noreferrer" class="link gold hover-orange pointer gt-walsheim-medium">Done Sunday <span>🌞</span></a></p>
             </div>
           </div>
         </div>
+        </Anime>
       </div>
     </div>
   </Layout>
