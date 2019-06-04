74a71c210e8de162613c9c40318205e4f5f2db45
Jack Veiga <hello@jackveiga.com>
2019-06-04 19:13:55 +0800

    Changes to colours and buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,19 +9,34 @@
             <img class="w-64" src="@/assets/svg/poketto-text.svg">
             <h2 class="mt-8 text-3xl gt-walsheim-medium leading-tight">A wallet that you can <br> actually show to your parents </h2>
             <div class="mt-4 text-lg open-sans-regular leading-snug">The simplest, most secure and near instant way <br> to pay friends and your every day items.</div>
-            <a
-              class="mt-8"
-              href="https://poketto.cash"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="App store link"
-            >
-              <img
-                alt="App store badge"
-                class="w-40 cursor-pointer"
-                src="@/assets/svg/app-store.svg"
-                >
-            </a>
+            <div class="inline-flex flex-row">
+              <a
+                class="mt-8"
+                href="https://poketto.cash"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="App store link"
+              >
+                <img
+                  alt="App store badge"
+                  class="w-40 cursor-pointer"
+                  src="@/assets/svg/app-store.svg"
+                  >
+              </a>
+              <a
+                class="mt-8 ml-8"
+                href="https://poketto.cash"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="App store link"
+              >
+                <img
+                  alt="App store badge"
+                  class="w-40 cursor-pointer"
+                  src="@/assets/svg/donation.svg"
+                  >
+              </a>
+            </div>
           </div>
           <div class="w-1/2 px-16">
             <img src="@/assets/img/intro/iphone@2x.png" alt="" />
@@ -75,7 +90,7 @@
           <div class="mt-32 flex flex-row items-center">
             <div class="flex flex-col max-w-sm pr-12">
               <h2 class="text-3xl header-color gt-walsheim-medium">Near instant payments</h2>
-              <p class="mt-2 text-xl open-sans-regular">By using xDai you can transfer instantaneously with a ridiculous low transfer fee* to another wallet.</p>
+              <p class="mt-2 text-xl open-sans-regular">By using xDai you can transfer instantaneously with a <span class="header-red underline cursor-pointer">ridiculous low transfer fee</span>* to another wallet.</p>
             </div>
             <div class="w-64 background--illustration">
               <img src="@/assets/img/illustrations/instant_payments.png" alt="" />
@@ -102,7 +117,7 @@
           <div class="mt-32 flex flex-row items-center">
             <div class="flex flex-col max-w-sm pr-12">
               <h2 class="text-3xl header-color gt-walsheim-medium">Completly open source</h2>
-              <p class="mt-2 text-xl open-sans-regular">Our wallet code is avaiable for anyone to check or contribute to on Github. Help us build the future!</p>
+              <p class="mt-2 text-xl open-sans-regular">Our wallet code is avaiable for anyone to check or <span span class="header-blue underline cursor-pointer">contribute to on Github</span>. Help us build the future!</p>
             </div>
             <div class="w-64 background--illustration">
               <img src="@/assets/img/illustrations/open_source.png" alt="" />
@@ -112,12 +127,49 @@
       </div>
     </div>
     <div class="flex background--alt">
-      <div class="container mx-auto flex flex-col items-center mt-32 mb-32">
+      <div class="container mx-auto flex flex-col items-center mt-32 mb-40">
         <div class="mt-16 inline-flex flex-row flex-no-wrap items-center">
           <div class="w-1/2 px-12 flex flex-col items-start">
             <h1 class="text-5xl gt-walsheim-medium leading-tight">Poketto uses xDai</h1>
-            <div class="mt-4 text-xl open-sans-regular leading-snug">The simplest, most secure and near instant way to pay friends and your every day items.</div>
-            <button class="mt-4 bg-red-600">lol</button>
+            <div class="mt-4 text-lg open-sans-regular leading-normal mr-16">The xDai Chain is an Ethereum compatible sidechain with Dai as the native currency of the network, combining POA Network’s interoperability knowledge and technology with MakerDAO’s Dai, the permissionless, crypto-collateralized USD-stable coin.</div>
+            <div class="inline-flex flex-row">
+              <a
+                class="mt-8"
+                href="https://medium.com/poa-network/poa-network-partners-with-makerdao-on-xdai-chain-the-first-ever-usd-stable-blockchain-65a078c41e6a"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="App store link"
+              >
+                <img
+                  class="w-40 cursor-pointer"
+                  src="@/assets/svg/learn-more.svg"
+                  >
+              </a>
+              <a
+                class="mt-8 ml-8"
+                href="https://medium.com/@jaredstauffer/how-to-get-xdai-how-to-convert-dai-to-xdai-eth-dai-xdai-30a60e4b6641"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="App store link"
+              >
+                <img
+                  class="w-40 cursor-pointer"
+                  src="@/assets/svg/how-to-convert.svg"
+                  >
+              </a>
+            </div>
+            <h2 class="mt-12 text-3xl gt-walsheim-medium leading-tight">Still unsure? <br> We can help you onboard!</h2>
+            <a
+                class="mt-8"
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  class="w-40 cursor-pointer"
+                  src="@/assets/svg/help-onboard.svg"
+                  >
+              </a>
           </div>
           <div class="w-1/2 max-w-5xl px-16">
             <img src="@/assets/img/illustrations/xdai.png" alt="" />
