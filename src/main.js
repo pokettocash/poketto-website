690c33e3e63956bb6770c33d5ad55b1018c3b6df
Jack Veiga <hello@jackveiga.com>
2019-06-05 00:10:18 +0800

    Add popup for transaction price

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import VModal from "vue-js-modal";
+import Popover from 'vue-js-popover'
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/css/tailwind.css";
 
+Vue.use(Popover);
 Vue.use(VModal);
 Vue.config.productionTip = false;
 
