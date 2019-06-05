4c1a98e31412a672d5b1eac076f91a726c9a8977
Jack Veiga <hello@jackveiga.com>
2019-06-06 00:54:43 +0800

    Changes to resposiveness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,9 +68,9 @@
     </div>
     <div class="flex">
       <div
-        class="container mx-auto flex flex-col items-center mt-24 mb-24 md:mt-32 md:mb-32"
+        class="container mx-auto flex flex-col items-center mt-24 mb-24"
       >
-        <div class="w-5/6">
+        <div class="w-5/6 md:w-3/4">
           <img src="@/assets/img/illustrations/everyone.jpg" />
         </div>
         <h1
@@ -82,20 +82,20 @@
       </div>
     </div>
     <div id="wallet" class="flex px-8 md:px-0 background--alt">
-      <div class="mx-auto flex flex-col flex-no-wrap items-center mt-32 mb-32">
+      <div class="mx-auto flex flex-col flex-no-wrap items-center mt-24 mb-24">
         <h1
-          class="text-2xl md:text-4xl lg:text-5xl text-center gt-walsheim-medium leading-tight"
+          class="text-2xl md:text-4xl text-center gt-walsheim-medium leading-tight"
         >
           Introducing your new wallet
         </h1>
         <p
-          class="mt-4 text-center text-base md:text-xl lg:text-2xl open-sans-regular leading-snug md:max-w-5xl md:px-32"
+          class="mt-4 text-center text-base md:text-xl lg:text-xl open-sans-regular leading-snug md:max-w-4xl md:px-32"
         >
           You shouldn't have to read a manual to start using a payment wallet,
           it should be as easy and intuitive as any other app you used before.
         </p>
         <div
-          class="mt-24 md:max-w-7xl flex flex-row items-center justify-around -mx-16 md:-mx-10"
+          class="mt-16 md:max-w-7xl flex flex-row items-center justify-around -mx-16 md:-mx-10"
         >
           <div
             class="hidden md:block max-w-sm mr-6"
@@ -144,15 +144,15 @@
     </div>
     <div id="features" class="flex px-10 md:px-0 ">
       <div
-        class="container mx-auto flex flex-col items-center mt-24 mb-24 md:mt-32 md:mb-40"
+        class="container mx-auto flex flex-col items-center mt-24 mb-24 md:mb-32"
       >
         <h1
-          class="text-2xl md:text-4xl lg:text-5xl gt-walsheim-medium leading-tight"
+          class="text-2xl md:text-4xl gt-walsheim-medium leading-tight"
         >
           Why use this wallet?
         </h1>
         <p
-          class="mt-4 text-center text-base md:text-xl lg:text-2xl open-sans-regular leading-snug md:max-w-5xl md:px-32"
+          class="mt-4 text-center text-base md:text-xl open-sans-regular leading-snug md:max-w-4xl md:px-32"
         >
           We built our wallet with simplicity in mind from day one without
           sacrificing any convenience that you need to use your money.
@@ -165,11 +165,11 @@
               class="flex flex-col items-center md:items-start max-w-sm px-4 md:px-0 text-center md:text-left md:pr-12"
             >
               <h2
-                class="mt-8 md:mt-0 text-xl md:text-2xl lg:text-3xl header-color gt-walsheim-medium"
+                class="mt-8 md:mt-0 text-xl md:text-2xl md:px-4 header-color gt-walsheim-medium"
               >
                 Near instant payments
               </h2>
-              <p class="mt-2 text-base md:text-lg lg:text-xl open-sans-regular">
+              <p class="mt-2 text-base md:text-lg md:px-4 open-sans-regular">
                 By using xDai you can transfer instantaneously with a
                 <span
                   v-popover:foo
@@ -179,12 +179,11 @@
               </p>
               <popover
                 class="open-sans-regular text-base md:text-lg text-center px-4 -ml-8 mt-2"
-                :width="225"
+                :width="155"
                 :event="hover"
                 name="foo"
               >
-                $1 on every <br />
-                50,000 transactions!
+                0.00002$ per transaction!
               </popover>
             </div>
             <div class="w-3/4 md:w-48 lg:w-64 background--illustration">
@@ -201,11 +200,11 @@
               class="flex flex-col items-center md:items-start  max-w-sm px-4 md:px-0 text-center md:text-left md:pr-12"
             >
               <h2
-                class="mt-8 md:mt-0 text-xl md:text-2xl lg:text-3xl header-color gt-walsheim-medium"
+                class="mt-8 md:mt-0 text-xl md:text-2xl md:px-4 header-color gt-walsheim-medium"
               >
                 Wallet is on your phone
               </h2>
-              <p class="mt-2 text-base md:text-lg lg:text-xl open-sans-regular">
+              <p class="mt-2 text-base md:text-lg md:px-4 open-sans-regular">
                 All you need to get started is your phone. Our wallet generates
                 the key and stores it in your phone.
               </p>
@@ -221,11 +220,11 @@
               class="flex flex-col items-center md:items-start  max-w-sm px-4 md:px-0 text-center md:text-left md:pr-12"
             >
               <h2
-                class="mt-8 md:mt-0 text-xl md:text-2xl lg:text-3xl header-color gt-walsheim-medium"
+                class="mt-8 md:mt-0 text-xl md:text-2xl md:px-4 header-color gt-walsheim-medium"
               >
                 Addresses as contacts
               </h2>
-              <p class="mt-2 text-base md:text-lg lg:text-xl open-sans-regular">
+              <p class="mt-2 text-base md:text-lg md:px-4 open-sans-regular">
                 You can easily assign addresses to contacts using your wallet,
                 making it easier to control payments.
               </p>
@@ -244,11 +243,11 @@
               class="flex flex-col items-center md:items-start  max-w-sm px-3 md:px-0 text-center md:text-left md:pr-12"
             >
               <h2
-                class="mt-8 md:mt-0 text-xl md:text-2xl lg:text-3xl header-color gt-walsheim-medium"
+                class="mt-8 md:mt-0 text-xl md:text-2xl md:px-4 header-color gt-walsheim-medium"
               >
                 Completly open source
               </h2>
-              <p class="mt-2 text-base md:text-lg lg:text-xl open-sans-regular">
+              <p class="mt-2 text-base md:text-lg md:px-4 open-sans-regular">
                 Our wallet code is avaiable for anyone to check or contribute to
                 <a
                   href="https://github.com/pokettocash"
@@ -268,26 +267,24 @@
     </div>
     <div id="xdai" class="flex background--alt">
       <div
-        class="container mx-auto flex flex-col items-center mt-24 mb-24 md:mt-32 md:mb-40"
+        class="container mx-auto flex flex-col items-center mt-24 mb-24 md:mb-32"
       >
         <div
-          class="md:mt-16 inline-flex flex-col-reverse lg:flex-row flex-no-wrap items-center"
+          class="md:mt-16 inline-flex flex-col-reverse lg:flex-row px-6 md:px-0 flex-no-wrap items-center justify-center"
         >
-          <div class="md:w-1/2 px-6 md:px-12 flex flex-col md:items-start">
+          <div class="md:w-1/2 flex flex-col md:max-w-lg md:items-start md:pl-6 md:pr-8">
             <h1
-              class="mt-16 text-center md:text-left lg:mt-0 text-2xl md:text-4xl lg:text-5xl gt-walsheim-medium leading-tight"
+              class="mt-16 text-center md:text-left lg:mt-0 text-2xl md:text-4xl gt-walsheim-medium leading-tight"
             >
               Poketto uses xDai
             </h1>
+            <h2 class="mt-4 text-center md:text-left text-xl md:text-2xl gt-walsheim-regular leading-tight px-6 md:px-0">The xDai Chain is an Ethereum compatible sidechain with Dai, acting as the native currency of the network</h2>
             <div
-              class="mt-4 text-center md:text-left text-base md:text-base lg:text-lg open-sans-regular leading-normal lg:mr-16"
+              class="mt-4 text-center md:text-left text-base md:text-base lg:text-lg open-sans-regular leading-normal"
             >
-              The xDai Chain is an Ethereum compatible sidechain with Dai as the
-              native currency of the network, combining POA Network’s
-              interoperability knowledge and technology with MakerDAO’s Dai, the
-              permissionless, crypto-collateralized USD-stable coin.
+              Combining POA Network’s interoperability knowledge and technology with MakerDAO’s Dai, the permissionless, crypto-collateralized USD-stable coin.
             </div>
-            <div class="inline-flex flex-row">
+            <div class="mt-2 inline-flex flex-row">
               <a
                 class="mt-8 grow--button"
                 href="https://medium.com/poa-network/poa-network-partners-with-makerdao-on-xdai-chain-the-first-ever-usd-stable-blockchain-65a078c41e6a"
@@ -330,7 +327,7 @@
             </a>
           </div>
           <div
-            class="md:w-1/2 max-w-5xl px-20 md:px-16"
+            class="md:w-1/2 max-w-5xl px-20 md:px-20"
             v-scroll-reveal="{ delay: 1000, distance: '20%' }"
           >
             <img src="@/assets/img/illustrations/xdai.png" alt="" />
@@ -340,19 +337,19 @@
     </div>
     <div id="community" class="flex">
       <div
-        class="container mx-auto flex flex-col px-6 md:px-0 items-center mt-24 mb-24 md:mt-32 md:mb-32"
+        class="container mx-auto flex flex-col px-6 md:px-0 items-center mt-24 mb-24 md:mb-32"
       >
-        <div class="w-5/6 background--illustration">
+        <div class="w-5/6 md:w-3/4 background--illustration">
           <img src="@/assets/img/illustrations/done_sunday.jpg" alt="" />
         </div>
         <h1
-          class="mt-16 text-center md:text-left text-3xl md:text-4xl lg:text-5xl gt-walsheim-medium leading-tight"
+          class="mt-16 text-center md:text-left text-3xl md:text-4xl gt-walsheim-medium leading-tight"
           v-scroll-reveal="{ delay: 500, distance: '15%' }"
         >
           Supported by a community
         </h1>
         <p
-          class="mt-4 text-center text-base md:text-xl lg:text-2xl open-sans-regular leading-snug px-5 md:max-w-5xl md:px-32"
+          class="mt-4 text-center text-base md:text-xl open-sans-regular leading-snug px-5 md:max-w-5xl md:px-40"
           v-scroll-reveal="{ delay: 500, distance: '15%' }"
         >
           Poketto started as an idea within our
